3f734683483372a391b3ac0231b4822bc7596995
Patrick Ryan <pryan@stoneturn.com>
2021-03-06 16:40:43 -0500

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='docscraper',
-    version='0.1.0',
+    version='0.1.1',
     description='a web crawler to scrape documents from websites',
     long_description=long_description,
     long_description_content_type="text/markdown",
