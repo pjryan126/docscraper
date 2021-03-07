6b72a70dfa732513d0c2efffc0468ee170ab675f
Patrick Ryan <pryan@stoneturn.com>
2021-03-07 07:49:19 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 setup(
     name='docscraper',
     version='1.0.1',
-    description='a web crawler to scrape documents from websites',
+    description='A web crawler to scrape documents from websites',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://docscraper.readthedocs.io/en/latest/',
     author='Patrick Ryan',
-    author_email='pryan@stoneturn.com',
+    author_email='pjryan126@gmail.com',
     license='MIT',
     install_requires=[
         'openpyxl',
