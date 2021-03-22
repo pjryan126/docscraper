6e602857e5eb4053d278a9e3da17780a9486c9fa
Patrick Ryan <pryan@stoneturn.com>
2021-03-22 10:20:36 -0400

    add api.crawl to docscraper python api interface

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='docscraper',
-    version='2.0.1',
+    version='2.0.2',
     description='A web crawler to scrape documents from websites',
     long_description=long_description,
     long_description_content_type="text/markdown",
